82c7da1a16786a719eda397241277eed9b3ec1aa
102Luke395 <arzyl.ms@gmail.com>
2025-08-11 23:08:07 +0800

    Add files via upload
    Variable are used but not all types. I will create separate python file for all types of variables. For now, I only used inputs and variables string.

---
+++ variables.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+alias = input("What is your alias: ")
+tool = input("What is your favorite tool: ")
+
+print(f"Hello {name} A.K.A {alias}, you got a great tool which is {tool}")
